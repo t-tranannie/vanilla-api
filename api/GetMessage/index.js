10877851ad31a60539a5856fcfb860d244562397
t-tranannie <t-tranannie@microsoft.com>
2024-08-22 13:41:57 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API - Test Provisioned Storage Accounts 2" },
+    body: { text: "Hello from the API - Test BYOS" },
   };
 };
